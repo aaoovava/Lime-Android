d111d530bbcbd3db93c6826933169be0883e9b9c
wasiliylolita <WasiliyLolita6@gmail.com>
2023-12-23 00:20:06 +0100

    add buy screen design fix normis size but small,large,tab still stands

--- LimeApp/app/build/generated/data_binding_base_class_source_out/debug/out/com/example/limeapp/databinding/BuyActivityBinding.java
+++ LimeApp/app/build/generated/data_binding_base_class_source_out/debug/out/com/example/limeapp/databinding/BuyActivityBinding.java
@@ -6,6 +6,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.constraintlayout.widget.ConstraintLayout;
@@ -20,11 +22,101 @@ public final class BuyActivityBinding implements ViewBinding {
   @NonNull
   private final ConstraintLayout rootView;
 
-  @NonNull
-  public final ImageView imageView23;
+  /**
+   * This binding is not available in all configurations.
+   * <p>
+   * Present:
+   * <ul>
+   *   <li>layout-normal/</li>
+   * </ul>
+   *
+   * Absent:
+   * <ul>
+   *   <li>layout/</li>
+   * </ul>
+   */
+  @Nullable
+  public final TextView BuyTxtTop;
 
-  @NonNull
+  /**
+   * This binding is not available in all configurations.
+   * <p>
+   * Present:
+   * <ul>
+   *   <li>layout-normal/</li>
+   * </ul>
+   *
+   * Absent:
+   * <ul>
+   *   <li>layout/</li>
+   * </ul>
+   */
+  @Nullable
+  public final ImageView imageView21;
+
+  /**
+   * This binding is not available in all configurations.
+   * <p>
+   * Present:
+   * <ul>
+   *   <li>layout-normal/</li>
+   * </ul>
+   *
+   * Absent:
+   * <ul>
+   *   <li>layout/</li>
+   * </ul>
+   */
+  @Nullable
   public final ImageView imageView28;
+
+  /**
+   * This binding is not available in all configurations.
+   * <p>
+   * Present:
+   * <ul>
+   *   <li>layout-normal/</li>
+   * </ul>
+   *
+   * Absent:
+   * <ul>
+   *   <li>layout/</li>
+   * </ul>
+   */
+  @Nullable
+  public final ImageView imageView32;
+
+  /**
+   * This binding is not available in all configurations.
+   * <p>
+   * Present:
+   * <ul>
+   *   <li>layout-normal/</li>
+   * </ul>
+   *
+   * Absent:
+   * <ul>
+   *   <li>layout/</li>
+   * </ul>
+   */
+  @Nullable
+  public final ImageView imageView41;
+
+  /**
+   * This binding is not available in all configurations.
+   * <p>
+   * Present:
+   * <ul>
+   *   <li>layout-normal/</li>
+   * </ul>
+   *
+   * Absent:
+   * <ul>
+   *   <li>layout/</li>
+   * </ul>
+   */
+  @Nullable
+  public final ImageView imageView52;
 
   @NonNull
   public final LinearLayout linearLayout;
@@ -41,18 +133,111 @@ public final class BuyActivityBinding implements ViewBinding {
   @NonNull
   public final ImageView navVerefication;
 
-  private BuyActivityBinding(@NonNull ConstraintLayout rootView, @NonNull ImageView imageView23,
-      @NonNull ImageView imageView28, @NonNull LinearLayout linearLayout, @NonNull ImageView navBuy,
-      @NonNull ImageView navFirstScreen, @NonNull ImageView navProfile,
-      @NonNull ImageView navVerefication) {
+  /**
+   * This binding is not available in all configurations.
+   * <p>
+   * Present:
+   * <ul>
+   *   <li>layout-normal/</li>
+   * </ul>
+   *
+   * Absent:
+   * <ul>
+   *   <li>layout/</li>
+   * </ul>
+   */
+  @Nullable
+  public final ScrollView scrollView3;
+
+  /**
+   * This binding is not available in all configurations.
+   * <p>
+   * Present:
+   * <ul>
+   *   <li>layout-normal/</li>
+   * </ul>
+   *
+   * Absent:
+   * <ul>
+   *   <li>layout/</li>
+   * </ul>
+   */
+  @Nullable
+  public final TextView textView14;
+
+  /**
+   * This binding is not available in all configurations.
+   * <p>
+   * Present:
+   * <ul>
+   *   <li>layout-normal/</li>
+   * </ul>
+   *
+   * Absent:
+   * <ul>
+   *   <li>layout/</li>
+   * </ul>
+   */
+  @Nullable
+  public final TextView textView15;
+
+  /**
+   * This binding is not available in all configurations.
+   * <p>
+   * Present:
+   * <ul>
+   *   <li>layout-normal/</li>
+   * </ul>
+   *
+   * Absent:
+   * <ul>
+   *   <li>layout/</li>
+   * </ul>
+   */
+  @Nullable
+  public final TextView textView16;
+
+  /**
+   * This binding is not available in all configurations.
+   * <p>
+   * Present:
+   * <ul>
+   *   <li>layout-normal/</li>
+   * </ul>
+   *
+   * Absent:
+   * <ul>
+   *   <li>layout/</li>
+   * </ul>
+   */
+  @Nullable
+  public final TextView textView17;
+
+  private BuyActivityBinding(@NonNull ConstraintLayout rootView, @Nullable TextView BuyTxtTop,
+      @Nullable ImageView imageView21, @Nullable ImageView imageView28,
+      @Nullable ImageView imageView32, @Nullable ImageView imageView41,
+      @Nullable ImageView imageView52, @NonNull LinearLayout linearLayout,
+      @NonNull ImageView navBuy, @NonNull ImageView navFirstScreen, @NonNull ImageView navProfile,
+      @NonNull ImageView navVerefication, @Nullable ScrollView scrollView3,
+      @Nullable TextView textView14, @Nullable TextView textView15, @Nullable TextView textView16,
+      @Nullable TextView textView17) {
     this.rootView = rootView;
-    this.imageView23 = imageView23;
+    this.BuyTxtTop = BuyTxtTop;
+    this.imageView21 = imageView21;
     this.imageView28 = imageView28;
+    this.imageView32 = imageView32;
+    this.imageView41 = imageView41;
+    this.imageView52 = imageView52;
     this.linearLayout = linearLayout;
     this.navBuy = navBuy;
     this.navFirstScreen = navFirstScreen;
     this.navProfile = navProfile;
     this.navVerefication = navVerefication;
+    this.scrollView3 = scrollView3;
+    this.textView14 = textView14;
+    this.textView15 = textView15;
+    this.textView16 = textView16;
+    this.textView17 = textView17;
   }
 
   @Override
@@ -82,17 +267,23 @@ public final class BuyActivityBinding implements ViewBinding {
     // This is done to optimize the compiled bytecode for size and performance.
     int id;
     missingId: {
-      id = R.id.imageView23;
-      ImageView imageView23 = ViewBindings.findChildViewById(rootView, id);
-      if (imageView23 == null) {
-        break missingId;
-      }
+      id = R.id.BuyTxtTop;
+      TextView BuyTxtTop = ViewBindings.findChildViewById(rootView, id);
+
+      id = R.id.imageView21;
+      ImageView imageView21 = ViewBindings.findChildViewById(rootView, id);
 
       id = R.id.imageView28;
       ImageView imageView28 = ViewBindings.findChildViewById(rootView, id);
-      if (imageView28 == null) {
-        break missingId;
-      }
+
+      id = R.id.imageView32;
+      ImageView imageView32 = ViewBindings.findChildViewById(rootView, id);
+
+      id = R.id.imageView41;
+      ImageView imageView41 = ViewBindings.findChildViewById(rootView, id);
+
+      id = R.id.imageView52;
+      ImageView imageView52 = ViewBindings.findChildViewById(rootView, id);
 
       id = R.id.linearLayout;
       LinearLayout linearLayout = ViewBindings.findChildViewById(rootView, id);
@@ -124,8 +315,24 @@ public final class BuyActivityBinding implements ViewBinding {
         break missingId;
       }
 
-      return new BuyActivityBinding((ConstraintLayout) rootView, imageView23, imageView28,
-          linearLayout, navBuy, navFirstScreen, navProfile, navVerefication);
+      id = R.id.scrollView3;
+      ScrollView scrollView3 = ViewBindings.findChildViewById(rootView, id);
+
+      id = R.id.textView14;
+      TextView textView14 = ViewBindings.findChildViewById(rootView, id);
+
+      id = R.id.textView15;
+      TextView textView15 = ViewBindings.findChildViewById(rootView, id);
+
+      id = R.id.textView16;
+      TextView textView16 = ViewBindings.findChildViewById(rootView, id);
+
+      id = R.id.textView17;
+      TextView textView17 = ViewBindings.findChildViewById(rootView, id);
+
+      return new BuyActivityBinding((ConstraintLayout) rootView, BuyTxtTop, imageView21,
+          imageView28, imageView32, imageView41, imageView52, linearLayout, navBuy, navFirstScreen,
+          navProfile, navVerefication, scrollView3, textView14, textView15, textView16, textView17);
     }
     String missingId = rootView.getResources().getResourceName(id);
     throw new NullPointerException("Missing required view with ID: ".concat(missingId));
--- LimeApp/app/src/main/java/com/example/limeapp/Core/BuyScreen.java
+++ LimeApp/app/src/main/java/com/example/limeapp/Core/BuyScreen.java
@@ -4,15 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
 import android.app.Dialog;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.View;
+import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.example.limeapp.Holder.TextHolder;
 import com.example.limeapp.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
@@ -28,176 +33,32 @@ public class BuyScreen extends AppCompatActivity {
 
         ImageView nav_verefication = findViewById(R.id.nav_verefication);
         ImageView nav_profil = findViewById(R.id.nav_profile);
+        ImageView nav_first = findViewById(R.id.nav_firstScreen);
+        TextView TopText = findViewById(R.id.BuyTxtTop);
+        TextView TextView14 = findViewById(R.id.textView14);
+        TextView TextView15 = findViewById(R.id.textView15);
+        TextView TextView16 = findViewById(R.id.textView16);
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        WindowManager windowManager = (WindowManager) this.getSystemService(Context.WINDOW_SERVICE);
+        if (windowManager != null) {
+            windowManager.getDefaultDisplay().getMetrics(displayMetrics);
+            //Txt %
+            int screenHeight = displayMetrics.heightPixels;
+            int screenWidth = displayMetrics.widthPixels;
+            TextHolder.SetTextSize(screenHeight,TopText,0.035);
+            TextHolder.SetTextSize(screenHeight,TextView14,0.035);
+            TextHolder.SetTextSize(screenHeight,TextView15,0.035);
+            TextHolder.SetTextSize(screenHeight,TextView16,0.035);
+        }
 
-        ImageView MainInfoBut1 = findViewById(R.id.imageView28);
-        ImageView MainInfoBut2 = findViewById(R.id.imageView23);
-        MainInfoBut2.setOnClickListener(new View.OnClickListener() {
+
+        nav_first.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Dialog dialog = new Dialog(BuyScreen.this);
-                dialog.setContentView(R.layout.activity_main_info);
-                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
-                ImageView closeButton = dialog.findViewById(R.id.imageView39);
-                dialog.show();
-
-                ImageView mapBut = dialog.findViewById(R.id.imageView43);
-                ImageView instBut = dialog.findViewById(R.id.imageView50);
-                FloatingActionButton floatingActionButton2 = dialog.findViewById(R.id.floatingActionButton2);
-                ImageView faceBut = dialog.findViewById(R.id.imageView51);
-                FloatingActionButton floatingActionButton = dialog.findViewById(R.id.floatingActionButton);
-
-
-                ImageView num1 = dialog.findViewById(R.id.imageView48);
-                ImageView num2 = dialog.findViewById(R.id.imageView49);
-                num2.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        String dial = "tel:" + "0953836588";
-                        startActivity(new Intent(Intent.ACTION_DIAL, Uri.parse(dial)));
-                    }
-                });
-                num1.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        String dial = "tel:" + "0535556530";
-                        startActivity(new Intent(Intent.ACTION_DIAL, Uri.parse(dial)));
-                    }
-                });
-                floatingActionButton2.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        floatingActionButton2.setVisibility(View.INVISIBLE);
-                        instBut.setVisibility(View.VISIBLE);
-                        faceBut.setVisibility(View.VISIBLE);
-                        Animation fadeInAnimation = AnimationUtils.loadAnimation(BuyScreen.this, android.R.anim.fade_in);
-                        Animation fadeOutAnimation1 = AnimationUtils.loadAnimation(BuyScreen.this, android.R.anim.slide_out_right);
-                        instBut.startAnimation(fadeInAnimation);
-                        instBut.startAnimation(fadeInAnimation);
-                        floatingActionButton2.startAnimation(fadeOutAnimation1);
-                    }
-                });
-
-                faceBut.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://m.facebook.com/profile.php?id=100090329526191"));
-                        startActivity(browserIntent);
-                    }
-                });
-                instBut.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://instagram.com/lime_myrhorod?igshid=NTc4MTIwNjQ2YQ=="));
-                        startActivity(browserIntent);
-                    }
-                });
-                floatingActionButton.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        String dial = "tel:" + "0953836588";
-                        startActivity(new Intent(Intent.ACTION_DIAL, Uri.parse(dial)));
-                    }
-                });
-                mapBut.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://maps.app.goo.gl/WFztc1xyYPfoGPHr6"));
-                        startActivity(browserIntent);
-                    }
-                });
-
-
-                closeButton.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        dialog.dismiss();
-                    }
-                });
+                toFirstView();
             }
         });
 
-        MainInfoBut1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Dialog dialog = new Dialog(BuyScreen.this);
-                dialog.setContentView(R.layout.activity_main_info);
-                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
-                ImageView closeButton = dialog.findViewById(R.id.imageView39);
-                dialog.show();
-
-                ImageView mapBut = dialog.findViewById(R.id.imageView43);
-                ImageView instBut = dialog.findViewById(R.id.imageView50);
-                ImageView faceBut = dialog.findViewById(R.id.imageView51);
-                FloatingActionButton floatingActionButton = dialog.findViewById(R.id.floatingActionButton);
-                FloatingActionButton floatingActionButton2 = dialog.findViewById(R.id.floatingActionButton2);
-                ImageView num1 = dialog.findViewById(R.id.imageView48);
-                ImageView num2 = dialog.findViewById(R.id.imageView49);
-                num2.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        String dial = "tel:" + "0953836588";
-                        startActivity(new Intent(Intent.ACTION_DIAL, Uri.parse(dial)));
-                    }
-                });
-                num1.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        String dial = "tel:" + "0535556530";
-                        startActivity(new Intent(Intent.ACTION_DIAL, Uri.parse(dial)));
-                    }
-                });
-                floatingActionButton2.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        floatingActionButton2.setVisibility(View.INVISIBLE);
-                        instBut.setVisibility(View.VISIBLE);
-                        faceBut.setVisibility(View.VISIBLE);
-                        Animation fadeInAnimation = AnimationUtils.loadAnimation(BuyScreen.this, android.R.anim.fade_in);
-                        Animation fadeOutAnimation1 = AnimationUtils.loadAnimation(BuyScreen.this, android.R.anim.slide_out_right);
-                        instBut.startAnimation(fadeInAnimation);
-                        instBut.startAnimation(fadeInAnimation);
-                        floatingActionButton2.startAnimation(fadeOutAnimation1);
-                    }
-                });
-
-                faceBut.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://m.facebook.com/profile.php?id=100090329526191"));
-                        startActivity(browserIntent);
-                    }
-                });
-                instBut.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://instagram.com/lime_myrhorod?igshid=NTc4MTIwNjQ2YQ=="));
-                        startActivity(browserIntent);
-                    }
-                });
-                floatingActionButton.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        String dial = "tel:" + "0953836588";
-                        startActivity(new Intent(Intent.ACTION_DIAL, Uri.parse(dial)));
-                    }
-                });
-                mapBut.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://maps.app.goo.gl/WFztc1xyYPfoGPHr6"));
-                        startActivity(browserIntent);
-                    }
-                });
-
-
-                closeButton.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        dialog.dismiss();
-                    }
-                });
-            }
-        });
         nav_profil.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -222,6 +83,11 @@ public class BuyScreen extends AppCompatActivity {
 
     void toProfile() {
         Intent intent = new Intent(this, ProfileActivity.class);
+        startActivity(intent);
+        overridePendingTransition(R.anim.anime_in, R.anim.anime_out);
+    }
+    void toFirstView(){
+        Intent intent = new Intent(this,FirstScreen.class);
         startActivity(intent);
         overridePendingTransition(R.anim.anime_in, R.anim.anime_out);
     }
